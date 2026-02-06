e825ca979b63fd573e6cd52089ae5e1cf0b3d92c
Shoni Pal <100448741+Shoni182@users.noreply.github.com>
2026-02-06 11:36:14 +0200

    1

--- src/src/components/App.tsx
+++ src/src/components/App.tsx
@@ -2,28 +2,44 @@ import "./App.css";
 import axios from "axios";
 import { useQuery } from "@tanstack/react-query";
 import TodosList from "./ArticleList/TodoList";
-
-
-interface Todo {
-  userId: number;
-  id: number;
-  title: string; 
-  completed: boolean;
-}
-
-const fetchTodos = async (id:number) => {
-  const response = await axios.get<Todo[]>("https://jsonplaceholder.typicode.com/todos",
-    {
-      params: {
-        id,
-      }
-    }
-  );
- return response.data
-}
+import SearchBar from "./SearchForm/SearchBar";
+import { type List } from "../types/list";
+import { useState } from "react";
 
 export default function App() {
+  // interface Todo {
+  //   userId: number;
+  //   id: number;
+  //   title: string;
+  //   completed: boolean;
+  // }
+
+  const fetchTodos = async (id: number) => {
+    const response = await axios.get<List[]>(
+      "https://jsonplaceholder.typicode.com/todos",
+      {
+        params: {
+          id,
+        },
+      },
+    );
+    return response.data;
+  };
+  const [id, setId] = useState(0);
+  const handleSearch = (id: number) => {
+    setId(id);
+  };
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ["Tasks", id],
+    queryFn: () => fetchTodos(id),
+  });
+
   return (
-  
-  )
+    <>
+      <SearchBar onSubmit={handleSearch} />
+      {isLoading && <p>Загрузка. . . </p>}
+      {isError && <p>Помилка!</p>}
+      {data && <TodosList items={data} />}
+    </>
+  );
 }
--- src/src/components/SearchForm/SearchBar.tsx
+++ src/src/components/SearchForm/SearchBar.tsx
@@ -1,22 +1,18 @@
-import { Formik } from "formik";
+// import { Formik } from "formik";
 
 interface SearchBarprop {
-  onSubmit: (query: string) => void;
+  onSubmit: (id: number) => void;
 }
 
 export default function SearchBar({ onSubmit }: SearchBarprop) {
   const handleSearch = (formData: FormData) => {
-    const query = formData.get("query") as string;
-    if (!query.trim()) {
-      alert("Please enter your search query.");
-      return;
-    }
-    onSubmit(query);
+    const id = formData.get("id") as unknown as number;
+    onSubmit(id);
   };
   return (
     <>
       <form action={handleSearch}>
-        <input type="text" name="query" placeholder="Search tasks" autoFocus />
+        <input type="text" name="id" placeholder="Search tasks" autoFocus />
         <button type="submit">Search</button>
       </form>
     </>
